f426eaf6709aec0777064f8b5c513a4bbf891418
Tri-Nguyen-Dev <minhtri1314@gmail.com>
2021-10-29 07:22:26 +0700

    first commit

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,15 +1,18 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 const connectDb = async () => {
-    try {
-        await mongoose.connect(process.env.MONGOURL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-            useCreateIndex: true
-        });
-        console.log("Connect database successfully")
-    } catch (error) {
-        console.log("Connect failure!!!", error)
-    }
-}
-export default connectDb
+  try {
+    await mongoose.connect(
+      "mongodb+srv://tringuyen:trivip007@cluster0.l5dg2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+      }
+    );
+    console.log("Connect database successfully");
+  } catch (error) {
+    console.log("Connect failure!!!", error);
+  }
+};
+export default connectDb;
